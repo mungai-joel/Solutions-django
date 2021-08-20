7a7b5be7644f47aae0ba2c00308f4ae57a2e84ed
mungai-joel <mungaijoel123@gmail.com>
2021-08-20 08:51:34 +0300

    requirements

--- simple/forms.py
+++ simple/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 from .models import Contact, Post, Comment
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
+
 
 
 # from django.db import transaction
